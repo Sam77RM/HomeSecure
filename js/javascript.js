413c31ade4aff20701d352ba09ba4a5d8a28ba0d
Sam77RM <smerar2023@gmail.com>
2026-09-01 15:39:16 -0500

    todas las demas paginas

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+    function activarSistema() {
+        alert("Sistema de seguridad ACTIVADO");
+    }
+
+    function modoCasa() {
+        alert("Modo EN CASA activado");
+    }
+
+    function activarSirena() {
+        alert("⚠ Sirena activada manualmente");
+    }
+
+    function desactivarSistema() {
+        alert("Sistema de seguridad DESACTIVADO");
+    }
